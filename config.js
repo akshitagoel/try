22a87a80b96f074bf72ae53311f1a789738e81fe
akshita <akshitagoel88@gmail.com>
2018-02-08 21:09:22 +0530

    m
    Signed-off-by: akshita <akshitagoel88@gmail.com>

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const PORT=process.env.PORT || 3232;
 const DATABASE_URL=process.env.DATABASE_URL ||
     //"postgres://ppuser:pppass@localhost:5432/passportdb"
+
     "mysql://ppuser:pppass@localhost:3306/passportdb"
 exports=module.exports={PORT,DATABASE_URL}
 
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const passport = require('./passport')
 const models = require('./db/models');
 const config= require('./config')
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
