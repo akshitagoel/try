1b1124668d80abb252a3f8cf576234d9f69f398e
akshita <akshitagoel88@gmail.com>
2018-02-08 21:44:05 +0530

    h
    Signed-off-by: akshita <akshitagoel88@gmail.com>

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const PORT=process.env.PORT || 3232;
 const DATABASE_URL=process.env.DATABASE_URL ||
     //"postgres://ppuser:pppass@localhost:5432/passportdb"
 
-    "mysql://ppuser:pppass@localhost:3306/passportdb"
+   // "mysql://ppuser:pppass@localhost:3306/passportdb"
+    "mysql://userauth:userauth@localhost:3306/userauth"
 exports=module.exports={PORT,DATABASE_URL}
 
 
